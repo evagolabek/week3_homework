b1cbca9593ffc0da9271af4176562b282ed855ac
evagolabek <golabek.eva@gmail.com>
2018-03-04 18:47:03 +0100

    added create and edit events

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
+app.use(bodyParser.json())
 
 var Sequelize = require('sequelize')
 var sequelize = new Sequelize('postgres://localhost:5432/postgres')
@@ -29,12 +31,43 @@ app.get('/events/:id', (request, response) => {
 app.get('/events', (request, response) => {
   Event.findAll({
     attributes: ['title', 'start_date', 'end_date'],
-    where: {
-      start_date: {
-        [Op.gte]: Date.now()
-      }
+    where: { start_date: { [Op.gte]: Date.now() }
     }})
     .then(events => {
-    response.send({ events })
+      response.send({ events })
   })
+})
+
+app.post('/events', (request, response) => {
+  const event = request.body
+
+  // couldn't compare with Date.now()
+  if(event.start_date > event.end_date) {
+    response.send('start_date must be before the end date')
+  }
+  else {
+    Event.create(event).then(event => {
+      response.status(201).send(event)
+    })
+  }
+})
+
+app.put('/events/:id', (request, response) => {
+  const eventId = Number(request.params.id)
+  const updates = request.body
+
+  Event.findById(request.params.id)
+    .then(event => {
+      return event.update(updates)
+    })
+    .then(event => {
+      response.send('the event has been updated')
+    })
+    .catch(error => {
+      response.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+
 })
